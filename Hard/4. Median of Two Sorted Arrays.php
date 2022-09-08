a04b408c6a45174de8fd9367df6d4185ccf17612
bkhorvat <bkhorvat.php@gmail.com>
2022-09-08 15:09:42 +0200

    4. Median of Two Sorted Arrays

---
+++ Hard/4. Median of Two Sorted Arrays.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *Given two sorted arrays nums1 and nums2 of size m and n respectively, return
+ * the median of the two sorted arrays.
+ *
+ * The overall run time complexity should be O(log (m+n)).
+ */
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Float
+     */
+    function findMedianSortedArrays($nums1, $nums2) {
+        $nums = array_merge($nums1, $nums2);
+        sort($nums, SORT_NUMERIC);
+        $countNums = count($nums);
+
+        if($countNums%2 == 0) {
+            $median = ($nums[$countNums/2] + $nums[$countNums/2-1])/2;
+            return $median;
+        }else {
+            $median = $nums[(($countNums-1)/2)];
+            return $median;
+        }
+    }
+}
