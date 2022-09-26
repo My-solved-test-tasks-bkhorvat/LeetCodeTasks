791d2c1dfcb7bd225b2f79cb63940db8b74dc30d
bkhorvat <bkhorvat.php@gmail.com>
2022-09-26 07:16:47 +0200

    Palindrome Number MySolution

---
+++ Easy/9. Palindrome Number/MySolution.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        if ($x == strrev($x)) {
+            return true;
+        }
+    }
+}
