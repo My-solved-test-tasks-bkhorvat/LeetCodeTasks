080454c4d6ce5ede533bd87892561e25021c82b1
bkhorvat <bkhorvat.php@gmail.com>
2022-11-01 18:29:55 +0100

    20. Valid Parentheses MySolution

---
+++ Easy/20. Valid Parentheses/MySolution.php
@@ -0,0 +1,28 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isValid($s) {
+        if (strlen($s) % 2) {
+            return false;
+        }
+
+        while($s) {
+            $s = str_replace("()", "", $s);
+            $s = str_replace("[]", "", $s);
+            $s = str_replace("{}", "", $s);
+
+            if ($cheked === $s) {
+                return false;
+            }
+            $cheked = $s;
+
+        }
+        var_dump($s);
+        return true;
+    }
+}
